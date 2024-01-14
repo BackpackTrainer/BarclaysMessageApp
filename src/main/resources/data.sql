c266f003bb12b8ab9f99c00679154931039886fd
BackpackTrainer <bill@fairfieldco.com>
2024-01-14 18:47:03 -0500

    works on a MySQL database once

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
-INSERT INTO person (id, name, email) values(100, 'fred', 'fred@gmail.com');
-INSERT INTO person (id, name, email) values(200, 'ethel', 'ethel@gmail.com');
-INSERT INTO person (id, name, email) values(300, 'lucy', 'lucy@gmail.com');
-INSERT INTO person (id, name, email) values(400, 'ricky', 'ricky@gmail.com');
-
-INSERT INTO message (id, content, sender_id) VALUES (150, 'First random message', 100);
-INSERT INTO message (id, content, sender_id) VALUES (250, 'Second random message', 100);
-INSERT INTO message (id, content, sender_id) VALUES (350, 'Third random message', 200);
-INSERT INTO message (id, content, sender_id) VALUES (450, 'Fourth random message', 200);
+# INSERT INTO person (id, name, email) values(100, 'fred', 'fred@gmail.com');
+# INSERT INTO person (id, name, email) values(200, 'ethel', 'ethel@gmail.com');
+# INSERT INTO person (id, name, email) values(300, 'lucy', 'lucy@gmail.com');
+# INSERT INTO person (id, name, email) values(400, 'ricky', 'ricky@gmail.com');
+#
+# INSERT INTO message (id, content, sender_id) VALUES (150, 'First random message', 100);
+# INSERT INTO message (id, content, sender_id) VALUES (250, 'Second random message', 100);
+# INSERT INTO message (id, content, sender_id) VALUES (350, 'Third random message', 200);
+# INSERT INTO message (id, content, sender_id) VALUES (450, 'Fourth random message', 200);
+#
