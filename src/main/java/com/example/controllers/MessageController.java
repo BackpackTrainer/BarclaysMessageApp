0a4f519b018fe6d718d8c5df331d43259b049b61
thousandemon <william@fairfieldco.com>
2024-01-22 16:33:50 -0500

    Added CrossOrigin Annotation to Controllers

--- src/main/java/com/example/controllers/MessageController.java
+++ src/main/java/com/example/controllers/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class MessageController {
     MessageService messageService;
 
--- src/main/java/com/example/controllers/PersonController.java
+++ src/main/java/com/example/controllers/PersonController.java
@@ -3,14 +3,12 @@ package com.example.controllers;
 import com.example.entity.Person;
 import com.example.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
 
 @RestController
+@CrossOrigin
 public class PersonController {
     PersonService personService;
 
